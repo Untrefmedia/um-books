402c22253070c280eb107240a043a3d63279ef28
pablo <pcoronel@untref.edu.ar>
2019-01-16 12:55:56 -0300

    renomvrado ruta dataList

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
-Route::group(['prefix' => 'admin', 'middleware' => ['web', 'admin', 'auth:admin']], function () {
-    Route::resource('venue', 'Untrefmedia\UMBooks\App\Http\Controllers\Admin\VenueController');
-    Route::get('venue.dataList', 'Untrefmedia\UMBooks\App\Http\Controllers\Admin\VenueController@dataList')->name('venue.dataList');
+Route::group(['prefix' => 'admin'], function () {
+    // Guardar datos del formulario público
 
-    Route::resource('book', 'Untrefmedia\UMBooks\App\Http\Controllers\Admin\BookController');
-    Route::get('book.dataList', 'Untrefmedia\UMBooks\App\Http\Controllers\Admin\BookController@dataList')->name('book.dataList');
+    Route::group(['middleware' => ['web', 'admin', 'auth:admin']], function () {
+        Route::resource('venue', 'Untrefmedia\UMBooks\App\Http\Controllers\Admin\VenueController');
+        Route::get('venueDataList', 'Untrefmedia\UMBooks\App\Http\Controllers\Admin\VenueController@dataList')->name('venue.dataList');
 
-    Route::resource('event', 'Untrefmedia\UMBooks\App\Http\Controllers\Admin\EventController');
-    Route::get('event.dataList', 'Untrefmedia\UMBooks\App\Http\Controllers\Admin\EventController@dataList')->name('event.dataList');
+        Route::resource('book', 'Untrefmedia\UMBooks\App\Http\Controllers\Admin\BookController');
+        Route::get('bookDataList', 'Untrefmedia\UMBooks\App\Http\Controllers\Admin\BookController@dataList')->name('book.dataList');
+
+        Route::resource('event', 'Untrefmedia\UMBooks\App\Http\Controllers\Admin\EventController');
+        Route::get('eventDataList', 'Untrefmedia\UMBooks\App\Http\Controllers\Admin\EventController@dataList')->name('event.dataList');
+    });
 });
 
+// Formulario público
 Route::get('book/form', 'Untrefmedia\UMBooks\App\Http\Controllers\BookController@form')->name('book.form');
