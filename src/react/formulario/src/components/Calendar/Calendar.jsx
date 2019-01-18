f3c69f33ff8e14a1044d5ea32ff329fe93269b0f
pablo <pcoronel@untref.edu.ar>
2019-01-18 18:34:35 -0300

    fullcalendar, eventos recurrentes

--- src/react/formulario/src/components/Calendar/Calendar.jsx
+++ src/react/formulario/src/components/Calendar/Calendar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Calendar as fullcalendar } from 'fullcalendar';
+import '../../../node_modules/fullcalendar/dist/plugins/rrule.js'; // need this! or include <script> tag instead
 import './fullcalendar.min.css';
 
 const Calendar = ({ selectedEvent }) => {
@@ -14,6 +15,72 @@ const Calendar = ({ selectedEvent }) => {
 
 	useEffect(() => {
 		var calendarEl = document.getElementById('calendar');
+
+		const eventos = [
+			{
+				title: 'Desde las 10',
+				rrule: {
+					freq: 'DAILY',
+					byweekday: ['mo', 'tu', 'we', 'th', 'fr'],
+					dtstart: '2018-12-01T10:00:00'
+				},
+				duration: '01:00'
+			},
+			{
+				title: 'Desde las 11',
+				rrule: {
+					freq: 'DAILY',
+					byweekday: ['mo', 'tu', 'we', 'th', 'fr'],
+					dtstart: '2018-12-01T11:00:00'
+				},
+				duration: '01:00'
+			},
+			{
+				title: 'Desde las 14',
+				rrule: {
+					freq: 'DAILY',
+					byweekday: ['mo', 'tu', 'we', 'th', 'fr'],
+					dtstart: '2018-12-01T14:00:00'
+				},
+				duration: '01:00'
+			},
+			{
+				title: 'Desde las 15',
+				rrule: {
+					freq: 'DAILY',
+					byweekday: ['mo', 'tu', 'we', 'th', 'fr'],
+					dtstart: '2018-12-01T15:00:00'
+				},
+				duration: '01:00'
+			},
+			{
+				title: 'Desde las 16',
+				rrule: {
+					freq: 'DAILY',
+					byweekday: ['mo', 'tu', 'we', 'th', 'fr'],
+					dtstart: '2018-12-01T16:00:00'
+				},
+				duration: '01:00'
+			},
+			{
+				title: 'Desde las 17',
+				rrule: {
+					freq: 'DAILY',
+					byweekday: ['mo', 'tu', 'we', 'th', 'fr'],
+					dtstart: '2018-12-01T17:00:00'
+				},
+				duration: '01:00'
+			},
+			{
+				title: 'Fin de semana',
+				rrule: {
+					freq: 'DAILY',
+					byweekday: ['sa', 'su'],
+					dtstart: '2018-12-01T15:00:00'
+				},
+				duration: '01:00'
+			}
+		];
 
 		var calendar = new fullcalendar(calendarEl, {
 			header: {
@@ -45,62 +112,7 @@ const Calendar = ({ selectedEvent }) => {
 			eventClick: function(info) {
 				setEvent(info.event.start + '|' + info.event.end);
 			},
-			events: [
-				{
-					title: 'All Day Event',
-					start: '2018-12-01'
-				},
-				{
-					title: 'Long Event',
-					start: '2018-12-07',
-					end: '2018-12-10'
-				},
-				{
-					groupId: 999,
-					title: 'Repeating Event',
-					start: '2018-12-09T16:00:00'
-				},
-				{
-					groupId: 999,
-					title: 'Repeating Event',
-					start: '2018-12-16T16:00:00'
-				},
-				{
-					title: 'Conference',
-					start: '2018-12-11',
-					end: '2018-12-13'
-				},
-				{
-					title: 'Meeting',
-					start: '2018-12-12T10:30:00',
-					end: '2018-12-12T12:30:00'
-				},
-				{
-					title: 'Lunch',
-					start: '2018-12-12T12:00:00'
-				},
-				{
-					title: 'Meeting',
-					start: '2018-12-12T14:30:00'
-				},
-				{
-					title: 'Happy Hour',
-					start: '2018-12-12T17:30:00'
-				},
-				{
-					title: 'Dinner',
-					start: '2018-12-12T20:00:00'
-				},
-				{
-					title: 'Birthday Party',
-					start: '2018-12-13T07:00:00'
-				},
-				{
-					title: 'Click for Google',
-					url: 'http://google.com/',
-					start: '2018-12-28'
-				}
-			]
+			events: eventos
 		});
 
 		calendar.render();
