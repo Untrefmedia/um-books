38599c57c5ff17c06cc22db66cb9f33f1afcd058
pablo <pcoronel@untref.edu.ar>
2019-02-07 12:13:31 -0300

    gif de carga

---
+++ src/resources/views/form.blade.php
@@ -0,0 +1,17 @@
+@extends('umadmin::admin.layouts.app')
+
+@section('title', 'Form')
+
+@section('css')
+    <link rel="stylesheet" type="text/css" href={{ asset('js/formulario/build/static/css/main.e7584eb9.chunk.css') }} />
+@endsection
+
+@section('content')
+    <div id="root">
+        <img src="https://thumbs.gfycat.com/ImpoliteLivelyGenet-size_restricted.gif" />
+    </div>
+@stop
+
+@section('js')
+    <script src={{ asset('js/formulario/build/static/js/runtime~main.229c360f.js') }}></script>
+@stop
