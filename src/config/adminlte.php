97b72c87683b8d24716858ee18950cc4c8a0bd75
pablo <pcoronel@untref.edu.ar>
2019-02-14 17:46:03 -0300

    key explicita

--- src/config/adminlte.php
+++ src/config/adminlte.php
@@ -1,73 +1,79 @@
 <?php
 
-return [
-    'menu' => [
+$venue = [
+    'text'        => 'Venues',
+    'url'         => 'admin/venue',
+    'icon'        => 'file',
+    'label'       => null,
+    'label_color' => 'success',
+    'submenu'     => [
         [
-            'text'        => 'Venues',
-            'url'         => 'admin/venue',
-            'icon'        => 'file',
-            'label'       => null,
-            'label_color' => 'success',
-            'submenu'     => [
-                [
-                    'text' => 'List',
-                    'url'  => 'admin/venue'
-                ],
-                [
-                    'text' => 'Create',
-                    'url'  => 'admin/venue/create'
-                ]
-            ]
+            'text' => 'List',
+            'url'  => 'admin/venue'
         ],
         [
-            'text'        => 'Books',
-            'url'         => 'admin/book',
-            'icon'        => 'file',
-            'label'       => null,
-            'label_color' => 'success',
-            'submenu'     => [
-                [
-                    'text' => 'List',
-                    'url'  => 'admin/book'
-                ],
-                [
-                    'text' => 'Create',
-                    'url'  => 'admin/book/create'
-                ]
-            ]
+            'text' => 'Create',
+            'url'  => 'admin/venue/create'
+        ]
+    ]
+];
+
+$book = [
+    'text'        => 'Books',
+    'url'         => 'admin/book',
+    'icon'        => 'file',
+    'label'       => null,
+    'label_color' => 'success',
+    'submenu'     => [
+        [
+            'text' => 'List',
+            'url'  => 'admin/book'
         ],
         [
-            'text'        => 'Events',
-            'url'         => 'admin/event',
+            'text' => 'Create',
+            'url'  => 'admin/book/create'
+        ]
+    ]
+];
+
+$event = [
+    'text'        => 'Events',
+    'url'         => 'admin/event',
+    'icon'        => 'file',
+    'label'       => null,
+    'label_color' => 'success',
+    'submenu'     => [
+        [
+            'text' => 'List',
+            'url'  => 'admin/event'
+        ],
+        [
+            'text' => 'Create',
+            'url'  => 'admin/event/create'
+        ],
+        ['text'       => 'Blocked Turns',
+            'url'         => 'admin/eventBlocked',
             'icon'        => 'file',
             'label'       => null,
             'label_color' => 'success',
             'submenu'     => [
                 [
                     'text' => 'List',
-                    'url'  => 'admin/event'
+                    'url'  => 'admin/eventBlocked'
                 ],
                 [
                     'text' => 'Create',
-                    'url'  => 'admin/event/create'
-                ],
-                ['text'       => 'Blocked Turns',
-                    'url'         => 'admin/eventBlocked',
-                    'icon'        => 'file',
-                    'label'       => null,
-                    'label_color' => 'success',
-                    'submenu'     => [
-                        [
-                            'text' => 'List',
-                            'url'  => 'admin/eventBlocked'
-                        ],
-                        [
-                            'text' => 'Create',
-                            'url'  => 'admin/eventBlocked/create'
-                        ]
-                    ]
+                    'url'  => 'admin/eventBlocked/create'
                 ]
             ]
         ]
+    ]
+];
+
+return [
+    'menu' => [
+        0 => $venue,
+        1 => $book,
+        2 => $event
     ]
 ];
